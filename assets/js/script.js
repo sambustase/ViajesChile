d7330eb56badf7abe67ecd0a6cabd491f07eae92
Samuel Bustamante <samuelcocinero2019@gmail.com>
2024-07-09 23:13:36 -0400

    Agregar funciones javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+// smooth scroll
 $(document).ready(function(){
     $("a").on('click', function(event) {
     if (this.hash !== "") {
@@ -12,3 +14,28 @@ $(document).ready(function(){
     }
     });
    });
+
+//    tooltip
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+//    alert cuando se hace click
+    // document.getElementById("#submitMessage");
+    $("#submitMessage").click(function(){
+        alert("Mensaje enviado");
+
+    });
+
+    // Desaparecer texto al hacer click en icono
+    $("#airPlane").on("click", function() {
+        $( "#textAirPlane" ).toggle("slow");
+    });
+
+    $("#mountain").on("click", function() {
+        $( "#textMountain" ).toggle("slow");
+    });  
+
+    $("#route").on("click", function() {
+        $( "#textRoute" ).toggle("slow");
+    });
